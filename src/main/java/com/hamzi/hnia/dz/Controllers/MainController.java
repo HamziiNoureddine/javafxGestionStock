82a4f5da782254b954b084b6a85e6f1d356467ed
HamziiNoureddine <you@example.comhamziinoureddine@gmail.com>
2025-02-15 17:27:51 +0100

    side menu javafx

--- src/main/java/com/hamzi/hnia/dz/Controllers/MainController.java
+++ src/main/java/com/hamzi/hnia/dz/Controllers/MainController.java
@@ -1,7 +1,50 @@
 package com.hamzi.hnia.dz.Controllers;
 
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Component
-public class MainController {
+public class MainController implements Initializable {
+    @FXML
+    private ImageView closeIcon;
+    @FXML
+    private Label Menu;
+    @FXML
+    private AnchorPane slider;
+    boolean isMenu = true;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        closeIcon.setOnMouseClicked(event->{
+
+            System.exit(0);
+        });
+
+        Menu.setOnMouseClicked(e->{
+
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.7));
+            slide.setNode(slider);
+            if (isMenu){
+                slide.setToX(-300);
+                slide.play();
+                slider.setTranslateX(0);
+            } else {
+                slide.setToX(0);
+                slide.play();
+                slider.setTranslateX(-300);
+            }
+           isMenu=!isMenu;
+        });
+
+
+    }
 }
